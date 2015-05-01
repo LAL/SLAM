d3b84c392d52c6a11146baaeb3c62bc6edbad81b
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-05-01 13:13:22 +0200

    Fix configuration for django-1.8.0   * Fix some default settings to have easiest installation      - Default database is db.sqlite3      - Default SECRET_KEY   * Fix quick_setup.sh as syncdb option no more exists      - no more need to overwrite configuration.py   * Fix css/js link from base.html      - link to /static/ instead of http://slam_uri/asset/   * Add French translation built by default      - No more need to rebuild it

--- quick_setup.sh
+++ quick_setup.sh
@@ -6,34 +6,10 @@ if [ ! -d src ]; then
     exit 1
 fi
 
-# write the default basic configuration
-echo 'Configuring the application...'
-mv ./src/configuration.py ./src/configuration.py.bak
-echo '
-import sys
-DEBUG = True
-ADMINS = ()
-DATABASES = {"default": {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": sys.path[0] + "/../slam.db"
-    }}
-TIME_ZONE = "Europe/Paris"
-LANGUAGE_CODE = "fr-FR"
-RELOAD_SCRIPT = ""
-' > ./src/configuration.py
-
-echo 'SECRET_KEY = r"""'"`</dev/urandom tr -dc '[:graph:]' | head -c50`"'"""' \
-    >> ./src/configuration.py
-
-echo 'ROOT_DIR = r"'"$(pwd)"'"' >> ./src/configuration.py
+cd src
 
 # create the django database
 echo 'Creation and initialization of the database...'
-python ./src/manage.py syncdb --noinput || exit 1
-
-# create the translation file for the web interface
-echo 'Compilation of the translation files...'
-cd ./src/webinterface
-python ../manage.py compilemessages
+python manage.py migrate || exit 1
 
 echo "Done. SLAM is ready."
